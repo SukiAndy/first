e37327a355c02e68a37180d9d348675ff39edae4
unknown <shbold88@gmail.com>
2023-10-30 15:24:07 +0800

    dskujhg

---
+++ app.js
@@ -0,0 +1,42 @@
+// undsen operatuudiin jishee
+
+var year, yearJohn, yearMark;
+
+year = 2023;
+ageJohn = 28;
+ageMark = 45;
+
+console.log('this year: ' + year);
+
+yearJohn = year - ageJohn;
+yearMark = year - ageMark;
+console.log('John born in ' + yearJohn);
+console.log("Mark born in " + yearMark);
+
+// after 15 years, how is the year?
+var future = year + 15
+console.log("after 15 years " + future + " th year")
+
+//18 jiliin daraa mark heden nastai baih ve?
+
+var ageMarkAfter18Year = ageMark + 18;
+console.log('18 jiliin daraa Markin nas: ' + ageMarkAfter18Year + " baina");
+
+console.log(year * 2);
+console.log(23 * 4);
+console.log(2 ** 2);
+console.log(8 % 3);
+
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+
+var name = "Boldoo";
+console.log(typeof name);
+var address = null;
+console.log(typeof address);
+
+console.log(3 / 2);
+console.log(3 / 'a')
+//NaN=Not A Number
